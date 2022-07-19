9d124369c733fb849cb12dfd38ab08708358e844
edivaldoleitao <52206423+edivaldoleitao@users.noreply.github.com>
2022-07-19 15:15:37 -0300

    Add files via upload

---
+++ canibais e missionarios/canibaisMissionarios.js
@@ -0,0 +1,16 @@
+const noh = Object()
+
+noh.estado = {
+    missionarios: 3,
+    canibais: 3,
+    margem: 'E'
+}
+noh.profundidade = 0
+noh.nopai = null
+
+
+function funcao() {
+    let valor = prompt('digite algo')
+    console.log(valor)
+}
+
